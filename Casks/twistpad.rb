5d266efd83fbbf26d3f534ffb1654c83d9c312f4
Toxic880 <cooledtechbusiness@gmail.com>
2026-08-16 17:59:50 +0100

    twistpad 1.2

--- Casks/twistpad.rb
+++ Casks/twistpad.rb
@@ -1,6 +1,6 @@
 cask "twistpad" do
-  version "1.1"
-  sha256 "00358c2c5dd61361e30c59329fa55d7e149b325b05a957ef5d33356958786505"
+  version "1.2"
+  sha256 "144e5e213bee0bbaa19551cd887d7dd03d295193a4fcf7216c4f5980676293f0"
 
   url "https://github.com/Toxic880/TwistPad/releases/download/v#{version}/TwistPad-#{version}.zip"
   name "TwistPad"
